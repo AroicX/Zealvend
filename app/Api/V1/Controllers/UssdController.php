7211343a3a47c09ce500fdbafb42fd676d75ded7
AroicX <Arowosegbe67@gmail.com>
2019-10-04 13:18:12 +0100

    ...

--- app/Api/V1/Controllers/UssdController.php
+++ app/Api/V1/Controllers/UssdController.php
@@ -240,7 +240,37 @@ class UssdController extends Controller
 
     public function ussdWebhook(Request $request)
     {
-       return response()->json($request->all());
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://telehost.requestcatcher.com/",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $request->all(),
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json",
+            "Postman-Token: 4cc96ece-9e1b-4e0c-a091-1cf0886724c4",
+            "cache-control: no-cache"
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+        echo "cURL Error #:" . $err;
+        } else {
+        echo $response;
+        }
+    //    return response()->json($request->all());
+
+       
     }
 
 
